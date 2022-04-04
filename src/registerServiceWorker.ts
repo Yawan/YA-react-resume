e03a8f964264cf3021a46c79625730facff7b835
Joe.Shieh <b97502027@gmail.com>
2022-04-04 23:54:34 +0800

    !disable service worker register

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -38,7 +38,7 @@ export default function register() {
         checkValidServiceWorker(swUrl)
       } else {
         // Is not local host. Just register service worker
-        registerValidSW(swUrl)
+        // registerValidSW(swUrl)
       }
     })
   }
